57ab6671f79fcc9b60b60e2413d160fd3b4c10f5
ezk0 <sahin.tekes@haw-hamburg.de>
2016-10-18 14:20:52 +0200

    Sensor: Lesen implementiert

--- src/aufgabe1/Messungen.java
+++ src/aufgabe1/Messungen.java
@@ -1,14 +1,21 @@
 package aufgabe1;
 
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class Messungen {
 	private double wert;
 	private LocalDateTime zeitstempel;
+	public String formattedDateTime;
+	
+	
 	
 	public double getWert(){
 		return wert;
 	}
+	
+
 	
 	public LocalDateTime getZeitstempel(){
 		return zeitstempel;
@@ -16,13 +23,14 @@ public class Messungen {
 	
 	public Messungen(double wert){
 		this.wert=wert;
-		zeitstempel=LocalDateTime.now();
-		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+		zeitstempel = LocalDateTime.now();
+		formattedDateTime = zeitstempel.format(formatter);		
 	}
 	
-//	public static void main(String[] args) {
-//		Messungen mess = new Messungen(5.4);
-//		System.out.println(mess.getZeitstempel());
-//	}
+	public static void main(String[] args) {
+		Messungen messung1 = new Messungen(5.4);
+		System.out.println(messung1.formattedDateTime);
+	}
 
 }
